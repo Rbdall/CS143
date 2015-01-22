04dc47e55f39a23957cbb75f1ba4c5024eb53541
Ryan Dall <Rbdall@ucla.edu>
2015-01-21 21:21:29 -0800

    toString implemented

--- CS143-lab1/src/java/simpledb/TupleDesc.java
+++ CS143-lab1/src/java/simpledb/TupleDesc.java
@@ -213,7 +213,10 @@ public class TupleDesc implements Serializable {
      * @return String describing this descriptor.
      */
     public String toString() {
-        // some code goes here
-        return "";
+        String result = "";
+        for(int i = 0; i < this.numFields(); i++){
+        	result += this.getFieldType(i) + " (" + this.getFieldName(i) + "), ";
+        }
+        return result;
     }
 }
