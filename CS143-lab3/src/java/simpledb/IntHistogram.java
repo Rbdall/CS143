aa4c5329f2515138766efb66f8cc434686c2bc2b
Ryan Dall <Rbdall@ucla.edu>
2015-03-11 18:37:43 -0700

    6/8 IntHistogram working

--- CS143-lab3/src/java/simpledb/IntHistogram.java
+++ CS143-lab3/src/java/simpledb/IntHistogram.java
@@ -1,5 +1,5 @@
 package simpledb;
-
+import java.util.*;
 /** A class to represent a fixed-width histogram over a single integer-based field.
  */
 public class IntHistogram {
@@ -20,8 +20,30 @@ public class IntHistogram {
      * @param min The minimum integer value that will ever be passed to this class for histogramming
      * @param max The maximum integer value that will ever be passed to this class for histogramming
      */
+	private int minVal;
+	private int maxVal;
+	private int bucketWidth = 0;
+	private ArrayList<ArrayList<Integer>> bucketList;
+	private ArrayList<Integer> bucketMinList;
+	private int numTuples = 0;
     public IntHistogram(int buckets, int min, int max) {
-    	// some code goes here
+    	minVal = min;
+    	maxVal = max;
+    	bucketWidth = (maxVal-minVal)/buckets;
+    	if(bucketWidth < 1){
+    		bucketWidth = 1;
+    	}
+    	
+    	int currBucketMin = minVal;
+    	bucketList = new ArrayList<ArrayList<Integer>>();
+    	bucketMinList = new ArrayList<Integer>();
+    	for(int i = 0; i < buckets; i++){
+    		bucketList.add(new ArrayList<Integer>());
+    		bucketMinList.add(currBucketMin);
+    		currBucketMin += bucketWidth;
+    	}
+
+    	
     }
 
     /**
@@ -29,7 +51,13 @@ public class IntHistogram {
      * @param v Value to add to the histogram
      */
     public void addValue(int v) {
-    	// some code goes here
+    	for(int i = 0; i < bucketMinList.size(); i++){
+    		if(v <= bucketMinList.get(i)){
+    			bucketList.get(i).add(v);
+    			numTuples++;
+    			return;
+    		}
+    	}
     }
 
     /**
@@ -43,9 +71,124 @@ public class IntHistogram {
      * @return Predicted selectivity of this particular operator and value
      */
     public double estimateSelectivity(Predicate.Op op, int v) {
-
-    	// some code goes here
-        return -1.0;
+    	int numInSmallerBuckets = 0;
+    	int numInLargerBuckets = 0;
+    	double result = -1.0;
+    	switch(op){
+    		case EQUALS:
+    			for(int i = 0; i < bucketMinList.size(); i++){
+    	    		if(v <= bucketMinList.get(i)){
+    	    			result = (bucketList.get(i).size()/bucketWidth)/numTuples;
+    	    			break;
+    	    		}
+    	    	}
+    			break;
+    		case NOT_EQUALS:
+    			for(int i = 0; i < bucketMinList.size(); i++){
+    	    		if(v <= bucketMinList.get(i)){
+    	    			result = (numTuples - (bucketList.get(i).size()/bucketWidth))/numTuples;
+    	    			break;
+    	    		}
+    	    	}
+    			break;
+    		case GREATER_THAN:
+    			if(v > maxVal){
+    				return 0.0;
+    			}
+    			else if (v < minVal){
+    				return 1.0;
+    			}
+    			for(int i = 0; i < bucketMinList.size(); i++){
+    	    		if(v <= bucketMinList.get(i)){
+    	    			numInLargerBuckets = 0; 
+    	    			for(int j = i+1; j < bucketList.size(); j++){
+    	    				numInLargerBuckets += bucketList.get(j).size();
+    	    			}
+    	    			int bRight = 0;
+    	    			if(i+1 == bucketMinList.size()){
+    	    				bRight = maxVal;
+    	    			}
+    	    			else{
+    	    				bRight = bucketMinList.get(i+1)-1;
+    	    			}
+    	    			double numToRightInBucket = ((bRight-v) * (bucketList.get(i).size()/bucketWidth));
+    	    			result = (numToRightInBucket+numInLargerBuckets)/numTuples;
+    	    			break;
+    	    		}
+    	    	}
+    			break;
+    		case GREATER_THAN_OR_EQ:
+    			if(v > maxVal){
+    				return 0.0;
+    			}
+    			else if (v < minVal){
+    				return 1.0;
+    			}
+    			for(int i = 0; i < bucketMinList.size(); i++){
+    	    		if(v <= bucketMinList.get(i)){
+    	    			numInLargerBuckets = 0; 
+    	    			for(int j = i+1; j < bucketList.size(); j++){
+    	    				numInLargerBuckets += bucketList.get(j).size();
+    	    			}
+    	    			int bRight = 0;
+    	    			if(i+1 == bucketMinList.size()){
+    	    				bRight = maxVal;
+    	    			}
+    	    			else{
+    	    				bRight = bucketMinList.get(i+1)-1;
+    	    			}
+    	    			double numToRightInBucket = (((bRight-v)+1) * (bucketList.get(i).size()/bucketWidth));
+    	    			result = (numToRightInBucket+numInLargerBuckets)/numTuples;
+    	    			break;
+    	    		}
+    	    	}
+    			break;
+    		case LESS_THAN:
+    			if(v > maxVal){
+    				return 1.0;
+    			}
+    			else if (v < minVal){
+    				return 0.0;
+    			}
+    			numInSmallerBuckets = 0;
+    			for(int i = 0; i < bucketMinList.size(); i++){
+    	    		if(v <= bucketMinList.get(i)){
+    	    			int bLeft = bucketMinList.get(i);
+    	    			double numToLeftInBucket = ((v-bLeft) * (bucketList.get(i).size()/bucketWidth));
+    	    			result = (numToLeftInBucket+numInSmallerBuckets)/numTuples;
+    	    			break;
+    	    		}
+    	    		else{
+    	    			numInSmallerBuckets += bucketList.get(i).size();
+    	    		}
+    	    	}
+    			break;
+    		case LESS_THAN_OR_EQ:
+    			if(v > maxVal){
+    				return 1.0;
+    			}
+    			else if (v < minVal){
+    				return 0.0;
+    			}
+    			numInSmallerBuckets = 0;
+    			for(int i = 0; i < bucketMinList.size(); i++){
+    	    		if(v <= bucketMinList.get(i)){
+    	    			int bLeft = bucketMinList.get(i);
+    	    			double numToLeftInBucket = (((v-bLeft)+1) * (bucketList.get(i).size()/bucketWidth));
+    	    			result = (numToLeftInBucket+numInSmallerBuckets)/numTuples;
+    	    			break;
+    	    		}
+    	    		else{
+    	    			numInSmallerBuckets += bucketList.get(i).size();
+    	    		}
+    	    	}
+    			break;
+    		default:
+    			return -1.0;
+    	}
+    	//System.out.println(result);
+    	return result;
+        
     }
     
     /**
@@ -66,8 +209,12 @@ public class IntHistogram {
      * @return A string describing this histogram, for debugging purposes
      */
     public String toString() {
-
-        // some code goes here
-        return null;
+    	String histo = "";
+    	for(int i = 0; i < bucketMinList.size(); i++){
+    		histo += "Bucket " + i + '\n';
+    		histo += '\t' + "Min Value: " + bucketMinList.get(i) + '\n';
+    		histo += '\t' + "Tuples in bucket: " + bucketList.get(i).size() + '\n';
+    	}
+        return histo;
     }
 }
